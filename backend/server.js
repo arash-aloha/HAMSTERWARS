265cd4d74bceeeb86853281a17c56f0316552fc2
arash-aloha <arash.a@live.se>
2021-10-27 19:48:24 +0100

    added PORT env variable

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,19 @@ const cors = require('cors');
 const app = express();
 
 //Configure server
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
 //Middleware
-app.use( cors() );
-app.use( express.json() )
+app.use( cors() )
+app.use( express.urlencoded({ extended: true }) );
+app.use( express.json() );
 app.use( (req, res, next) => {
     requestCount++
     console.log(`${requestCount}, Method: ${req.method}, Request URL: ${req.url}`, req.body)
     next();
 })
+//app.use( '/public', express.static(__dirname + '/../public') )
+//app.use( '/img', express.static(__dirname + '/../hamsters') )
 
 //Endpoints
 app.get('/', (req, res) => {
