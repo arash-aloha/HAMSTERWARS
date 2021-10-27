59453ac436361a9c3cce641c4c1d4838d0abfb97
arash-aloha <arash.a@live.se>
2021-10-27 20:29:23 +0100

    fixed path

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 1337;
 let reqCount = 0;
 
 //Middleware
-app.use( cors() )
+app.use( cors() );
 app.use( express.urlencoded({ extended: true }) );
 app.use( express.json() );
 app.use( (req, res, next) => {
     reqCount++
-    console.log(`${reqCount}, Method: ${req.method}, Request URL: ${req.url}`, req.body)
+    console.log(`${reqCount}, Method: ${req.method}, Request URL: ${req.url}`, req.body);
     next();
-})
-app.use( express.static(__dirname + '/build') )
+});
+app.use( express.static(__dirname + '/../build') )
 //app.use( '/img', express.static(__dirname + '/../hamsters') )
 
 //Endpoints
@@ -24,17 +24,14 @@ app.use( express.static(__dirname + '/build') )
 //     console.log('Server received GET / ');
 //     res.send('GET "/" response');
 // });
-app.get('/hamsters', (req,res) => {
-    console.log('Server received GET /hamsters');
-    res.send('GET "/HAMSTERS"')
-})
-
 app.get('*', (req, res) => {
     console.log('* is being called...')
     res.send(__dirname + '/build/index.html');
-})
-
-
+});
+app.get('/hamsters', (req,res) => {
+    console.log('Server received GET /hamsters');
+    res.send('GET "/HAMSTERS"')
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 });
