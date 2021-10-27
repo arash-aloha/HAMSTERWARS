3b6dfba43dddffca87a803bfd93ff9861ab6ebee
arash-aloha <arash.a@live.se>
2021-10-27 20:07:18 +0100

    server changes

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,33 @@ const app = express();
 
 //Configure server
 const PORT = process.env.PORT || 1337;
+let reqCount = 0;
 
 //Middleware
 app.use( cors() )
 app.use( express.urlencoded({ extended: true }) );
 app.use( express.json() );
 app.use( (req, res, next) => {
-    requestCount++
-    console.log(`${requestCount}, Method: ${req.method}, Request URL: ${req.url}`, req.body)
+    reqCount++
+    console.log(`${reqCount}, Method: ${req.method}, Request URL: ${req.url}`, req.body)
     next();
 })
-//app.use( '/public', express.static(__dirname + '/../public') )
+app.use( express.static(__dirname + '/build') )
 //app.use( '/img', express.static(__dirname + '/../hamsters') )
 
 //Endpoints
-app.get('/', (req, res) => {
-    console.log('Server received GET / ');
-    res.send('GET "/" response');
-});
+// app.get('/', (req, res) => {
+//     console.log('Server received GET / ');
+//     res.send('GET "/" response');
+// });
 app.get('/hamsters', (req,res) => {
     console.log('Server received GET /hamsters');
     res.send('GET "/HAMSTERS"')
+})
+
+app.get('*', (req, res) => {
+    console.log('* is being called...')
+    res.send(__dirname + '/build/index.html');
 })
 
 
